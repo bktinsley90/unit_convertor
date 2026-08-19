d250776e5d1c86b5b187e864987bd4c3aa146356
bktinsley90 <bktinsley90@gmail.com>
2026-08-19 18:52:14 -0400

    Add README file for Input Convertor project with description and image

---
+++ readme.md
@@ -0,0 +1,6 @@
+##Input Convertor - Scrimba Project
+
+![Input Convertor](inputConversion.png)
+
+### Description
+This project is a simple input converter that allows users to convert different types of inputs into various formats. It is built using HTML, CSS, and JavaScript.
--- script.js
+++ script.js
@@ -3,3 +3,47 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const convertBtn = document.querySelector('#convert-btn');
+const inputField = document.querySelector('input[type="number"]');
+const meterToFeetResult = document.querySelector('.meters-result');
+const litersToGallonsResult = document.querySelector('.liters-result');
+const kilogramsToPoundsResult = document.querySelector('.kilograms-result');
+
+convertBtn.addEventListener('click', function () {
+  //console.log('Button clicked');
+  // meter to feet conversion
+  const meters = parseFloat(inputField.value);
+  const feet = metersToFeet(meters);
+  // console.log('Meters:', meters, 'Feet:', feet);
+  meterToFeetResult.textContent = `${meters} meters = ${feet.toFixed(2)} feet | ${feet.toFixed(2)} feet = ${meters} meters`;
+  //liters to gallons conversion
+  const liters = parseFloat(inputField.value);
+  const gallons = litersToGallons(liters);
+  litersToGallonsResult.textContent = `${liters} liters = ${gallons.toFixed(2)} gallons | ${gallons.toFixed(2)} gallons = ${liters} liters`;
+  //kilograms to pounds conversion
+  const kilograms = parseFloat(inputField.value);
+  const pounds = kilogramsToPounds(kilograms);
+  kilogramsToPoundsResult.textContent = `${kilograms} kilograms = ${pounds.toFixed(2)} pounds | ${pounds.toFixed(2)} pounds = ${kilograms} kilograms`;
+});
+
+function metersToFeet(meters) {
+  return meters * 3.281;
+}
+
+function feetToMeters(feet) {
+  return feet / 3.281;
+}
+
+function litersToGallons(liters) {
+  return liters * 0.264;
+}
+function gallonsToLiters(gallons) {
+  return gallons / 0.264;
+}
+function kilogramsToPounds(kilograms) {
+  return kilograms * 2.204;
+}
+function poundsToKilograms(pounds) {
+  return pounds / 2.204;
+}
